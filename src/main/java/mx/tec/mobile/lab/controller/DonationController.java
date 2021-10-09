ec4c222bafe6889cac0fbed224713a7cd53a2008
PaulinaAldaco <paulina.aldaco.ro@gmail.com>
2021-10-09 12:22:57 -0700

    Adding donation export endpoint

---
+++ src/main/java/mx/tec/mobile/lab/controller/DonationController.java
@@ -0,0 +1,37 @@
+package mx.tec.mobile.lab.controller;
+
+import java.util.Calendar;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import mx.tec.mobile.lab.manager.DonationManager;
+import mx.tec.mobile.lab.vo.Donation;
+
+@RestController
+public class DonationController {
+	@Autowired
+	DonationManager manager;
+	
+	@GetMapping("/donation/{id}")
+	public Donation getDonation(@PathVariable(value = "id") long id) {
+		//return new Donation("Walmart", "Juventud", 15.2f, Calendar.getInstance());
+		//return manager.retrieveDonation(id).get();
+		//throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+		Optional<Donation> retrievedDonation = manager.retrieveDonation(id);
+		if (retrievedDonation.isEmpty()) {
+			//return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+		}
+		else {
+			//return new ResponseEntity<>(retrievedDonation.get(), HttpStatus.OK);
+			return retrievedDonation.get();
+		}
+	}
+}
