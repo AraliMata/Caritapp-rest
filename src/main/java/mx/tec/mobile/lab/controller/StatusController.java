9a756d539f3ff316bb3268aa5ece5f1803602d6d
andykandujo <a00227491@itesm.mx>
2021-10-23 07:03:19 -0700

    modified:   bin/src/main/java/mx/tec/mobile/lab/controller/LineaController.class

--- src/main/java/mx/tec/mobile/lab/controller/StatusController.java
+++ src/main/java/mx/tec/mobile/lab/controller/StatusController.java
@@ -1,5 +1,6 @@
 package mx.tec.mobile.lab.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import mx.tec.mobile.lab.manager.StatusManager;
-import mx.tec.mobile.lab.vo.Donation;
+import mx.tec.mobile.lab.vo.Status;
 
 @RestController
 public class StatusController {
@@ -22,11 +23,11 @@ public class StatusController {
 		// TODO Auto-generated constructor stub
 	}
 	
-	// Get a donation object by id
+	// Get a status object by id
 		@GetMapping("/status/{id}")
-		public Donation getDonation(@PathVariable(value = "id") long id) {
+		public Status getStatus(@PathVariable(value = "id") long id) {
 			// Query the database
-			Optional<Donation> retrievedStatus = manager.retrieveStatus(id);
+			Optional<Status> retrievedStatus = manager.retrieveStatus(id);
 			// Check if the query found something, and return http status code and content accordingly
 			if (!retrievedStatus.isPresent()) {
 				throw new ResponseStatusException(HttpStatus.NO_CONTENT);
@@ -35,7 +36,11 @@ public class StatusController {
 				return retrievedStatus.get();
 			}
 		}
-
-	
+		
+		
+		@GetMapping("/status/getStatus")
+		public List<Status> getStatuses(){
+			return manager.retrieveStatuses();
+		}
 
 }
--- src/main/java/mx/tec/mobile/lab/manager/StatusManager.java
+++ src/main/java/mx/tec/mobile/lab/manager/StatusManager.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import mx.tec.mobile.lab.repository.StatusRepository;
 import mx.tec.mobile.lab.vo.Status;
-import mx.tec.mobile.lab.vo.Donation;
 
 @Service
 public class StatusManager {
@@ -22,10 +21,13 @@ public class StatusManager {
 	
 	
 	// Find Donation by id
-		public Optional<Donation> retrieveStatus(long id) {
+		public Optional<Status> retrieveStatus(long id) {
 			return repository.findById(id); 
 		}
 		
+		public List<Status> retrieveStatuses(){
+			return repository.findAll();
+		}
 	
 
 }
